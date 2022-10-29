948987f838e7f10c65ad92c2573a1436934b02d9
Abhishek <abhishekparmar162@gmail.com>
2022-10-30 00:37:37 +0530

    complete pc website

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -10,13 +10,13 @@ const Hero = () => {
     <div className='flex justify-around'>
       <div className=''>
         <h1 className='text-7xl font-semibold '  >Make <br /> remote work</h1>
-        <p>Get your team in sync, no matter your location. <br/> Streamline processes, create team rituals, and <br />watch productivity soar.</p>
- <button className='bg-black text-white px-3 py-2 rounded-xl'>Learn More</button>
- <div className='flex'>
-    <img src={Databiz} alt="" />
-    <img src={AudioPhle} alt="" />
-    <img src={Meet} alt="" />
-    <img src={Maker} alt="" />
+        <p className='text-gray-500 py-8 text-2xl'>Get your team in sync, no matter your location. <br/> Streamline processes, create team rituals, and <br />watch productivity soar.</p>
+ <button className='bg-black hover:bg-white hover:text-black border border-black text-white px-8 mt-12 mb-16 py-4 rounded-xl'>Learn More</button>
+ <div className='flex mt-8'>
+    <img src={Databiz} alt="data" className='mr-4 w-24 h-12' />
+    <img src={AudioPhle} alt=""  className='mr-4 w-24 h-12'/>
+    <img src={Meet} alt="" className='mr-4 w-24 h-12'/>
+    <img src={Maker} alt="" className='mr-4 w-24 h-12'/>
  </div>
       </div>
       <div>
